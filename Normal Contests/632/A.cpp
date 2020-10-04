790b29f4c3e9f37c47162e25873a126d77ca97dc
Shambhav Goswami <56958751+shambhavgo@users.noreply.github.com>
2020-10-05 00:21:29 +0530

    Update A.cpp

--- Normal Contests/632/A.cpp
+++ Normal Contests/632/A.cpp
@@ -1,5 +1,30 @@
 #include<bits/stdc++.h>
+using namespace std;#include<bits/stdc++.h>
 using namespace std;
+#define inp(k)           ll k;cin>>k;
+#define in2(n, k)       ll n, k; cin>>n;cin>>k;
+#define ll long long
+#define dl double
+#define ch char
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define rf(i,a,b) for(ll i=a-1;i>=b;i--)
+#define Godspeed                ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define vpl vector< pair<ll,ll> >
+#define vpi vector< pair<int,int> >
+#define vi vector<int>
+#define vl vector<ll>
+#define vdl vector<dl>
+#define vch vector<char>
+#define pll pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define se second
+#define PI 3.14159265
+bool comp(pair<long long int ,long long int > &a,pair<long long int ,long long int > &b)
+{
+	return a.second > b.second;
+}
 int main()
 {
 	int t;
@@ -8,9 +33,9 @@ int main()
 	{
 		int a,b;
 		cin>>a>>b;
-		for(int i=0;i<a;i++)
+		f(i,0,a)
 		{
-			for(int j=0;j<b;j++)
+			f(i,0,b)
 			{
 				if(i==0&&j==0)
 				cout<<"W";
